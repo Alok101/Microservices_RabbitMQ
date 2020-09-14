4da1ee8422e4e2e0cd6b84748d487a6fd37c67c1
Alok101 <ijsb1098@gmail.com>
2020-09-14 23:29:41 +0530

    Partially Completed Banking Services

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+        public BankingController(IAccountService accountService, ILogger<BankingController> logger)
+        {
+            _logger = logger;
+            _accountService = accountService;
+        }
+
+        //Get api/banking
+        [HttpGet]
+        public  ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
--- MicroRabbit/MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Banking.Data.Context;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -22,5 +26,30 @@ namespace MicroRabbit.Banking.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+    public class BankingFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        //// Get environment
+        //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        //// Build config
+        //IConfiguration config = new ConfigurationBuilder()
+        //    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../EfDesignDemo.Web"))
+        //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        //    .AddEnvironmentVariables()
+        //    .Build();
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDbContext>();
+            var connectionString = configuration.GetConnectionString("BankingDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new BankingDbContext(optionsBuilder.Options);
+        }
     }
 }
