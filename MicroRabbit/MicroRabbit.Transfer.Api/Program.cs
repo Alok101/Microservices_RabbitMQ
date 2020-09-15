9642b62b016dd77a2e668774e67a3f334ccda262
Alok101 <ijsb1098@gmail.com>
2020-09-15 11:59:47 +0530

    Complete Code of Publish and Subscribe

--- MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Transfer.Api/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Transfer.Data.Context;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -22,5 +26,30 @@ namespace MicroRabbit.Transfer.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+    public class TransferFactory : IDesignTimeDbContextFactory<TransferDbContext>
+    {
+        //// Get environment
+        //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        //// Build config
+        //IConfiguration config = new ConfigurationBuilder()
+        //    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../EfDesignDemo.Web"))
+        //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        //    .AddEnvironmentVariables()
+        //    .Build();
+        public TransferDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<TransferDbContext>();
+            var connectionString = configuration.GetConnectionString("TransferDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new TransferDbContext(optionsBuilder.Options);
+        }
     }
 }
